570c124281d87d8ad7901b182e6edec82962750a
muhammadkasyfil <141574422+muhammadkasyfil@users.noreply.github.com>
2025-01-16 20:35:13 +0700

    letsgoo

--- calculabmobile/app/_layout.tsx
+++ calculabmobile/app/_layout.tsx
@@ -1,14 +1,23 @@
 import { Stack } from 'expo-router';
+import { useCallback } from 'react';
 import { useFonts } from 'expo-font';
 import * as SplashScreen from 'expo-splash-screen';
 import { useEffect } from 'react';
 import { View } from 'react-native';
+
+SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
   const [fontsLoaded, fontError] = useFonts({
     'Inter-Bold': require('../assets/fonts/Inter-Bold.ttf'),
     'Inter-Regular': require('../assets/fonts/Inter-Regular.ttf'),
   });
+
+  const onLayoutRootView = useCallback(async () => {
+    if (fontsLoaded) {
+      await SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded]);
 
   useEffect(() => {
     if (fontsLoaded || fontError) {
--- calculabmobile/metro.config.js
+++ calculabmobile/metro.config.js
@@ -1,17 +1,21 @@
 const { getDefaultConfig } = require('expo/metro-config');
 
 module.exports = (async () => {
-  const {
-    resolver: { sourceExts, assetExts }
-  } = await getDefaultConfig(__dirname);
+  const config = await getDefaultConfig(__dirname);
+  
+  config.resolver.assetExts.push('png');
+  config.resolver.assetExts = config.resolver.assetExts.filter((ext) => ext !== 'svg');
+  config.resolver.sourceExts.push('svg');
+  
   return {
+    ...config,
     transformer: {
+      ...config.transformer,
       babelTransformerPath: require.resolve('react-native-svg-transformer'),
       assetPlugins: ['expo-asset/tools/hashAssetFiles']
     },
     resolver: {
-      assetExts: assetExts.filter(ext => ext !== 'svg'),
-      sourceExts: [...sourceExts, 'svg']
+      ...config.resolver,
     }
   };
 })();
